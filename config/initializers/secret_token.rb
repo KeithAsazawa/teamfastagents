08ac1a7bd80a828685eb009f153237d8599ef2ea
KeithAsazawa <keith.asazawa@gmail.com>
2013-09-07 22:20:24 -0700

    First commit, remove public/index.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamfastagents::Application.config.secret_token = '2423e6e671924a07c48853a10cd5f7eb9070d6684203b4729e13bce3546264fa32e1b815f26a3453064cd345a0e334bdfd59134b02a00cdd35ec394636466c58'
