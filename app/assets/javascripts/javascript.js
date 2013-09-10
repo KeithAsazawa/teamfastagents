e10978e122ae0abbb5a7732df1f1502b20fc4bdc
KeithAsazawa <keith.asazawa@gmail.com>
2013-09-09 21:17:01 -0700

    Add incomplete footer

---
+++ app/assets/javascripts/javascript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#logoid').hover(
+		function() {
+		$(this).stop().animate({"opacity": "1"}, "slow");
+		},
+		function() {
+		$(this).stop().animate({"opacity": "0.3"}, "slow");
+	});
